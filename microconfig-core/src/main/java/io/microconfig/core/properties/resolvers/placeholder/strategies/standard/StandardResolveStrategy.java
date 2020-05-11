9dbdfaaf4273db96e2bd0ea61f14081638cb6e3b
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-05-11 17:20:03 +0300

    placeholder api changes

--- microconfig-core/src/main/java/io/microconfig/core/properties/resolvers/placeholder/strategies/standard/StandardResolveStrategy.java
+++ microconfig-core/src/main/java/io/microconfig/core/properties/resolvers/placeholder/strategies/standard/StandardResolveStrategy.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static io.microconfig.core.configtypes.ConfigTypeFilters.configTypeWithName;
-import static java.util.function.Function.identity;
 import static java.util.stream.Stream.of;
 
 @RequiredArgsConstructor
@@ -20,22 +21,34 @@ public class StandardResolveStrategy implements PlaceholderResolveStrategy {
                                       DeclaringComponent sourceOfValue,
                                       DeclaringComponent root,
                                       Set<Placeholder> visited) {
-        return of(
-                of(root),
-                visited.stream().map(v -> v.getReferencedComponent("this")), //todo
-                of(sourceOfValue)
-        ).flatMap(identity())
-                .map(DeclaringComponentImpl::copyOf).distinct()//for correct distinct
-                .map(c -> doResolve(c.getComponent(), p.getKey(), c.getEnvironment(), c.getConfigType())) //in some cases can't be override
+        return overridePriority(p, sourceOfValue, root, visited)
+                .map(c -> doResolve(c, p.getKey())) //in some cases can't be override
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .findFirst();
     }
 
-    private Optional<Property> doResolve(String component, String key, String environment, String configType) {
-        return environmentRepository.getOrCreateByName(environment)
-                .getOrCreateComponentWithName(component)
-                .getPropertiesFor(configTypeWithName(configType))
+    private Stream<DeclaringComponent> overridePriority(Placeholder p,
+                                                        DeclaringComponent sourceOfValue,
+                                                        DeclaringComponent root,
+                                                        Set<Placeholder> visited) {
+        if (!p.isSelfReferenced() && !p.referencedTo(sourceOfValue)) {
+            return Stream.of(p.getReferencedComponent(""));
+        }
+
+        return of(
+                of(root),
+                visited.stream().map(v -> v.getReferencedComponent("this")), //todo
+                of(sourceOfValue)
+        ).flatMap(Function.identity())
+                .map(DeclaringComponentImpl::copyOf)//for correct distinct
+                .distinct();
+    }
+
+    private Optional<Property> doResolve(DeclaringComponent c, String key) {
+        return environmentRepository.getOrCreateByName(c.getEnvironment())
+                .getOrCreateComponentWithName(c.getComponent())
+                .getPropertiesFor(configTypeWithName(c.getConfigType()))
                 .getPropertyWithKey(key);
     }
 }
