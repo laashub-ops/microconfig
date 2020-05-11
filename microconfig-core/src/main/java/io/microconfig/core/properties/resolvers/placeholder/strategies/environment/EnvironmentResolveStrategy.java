9dd3c8fff100065256e525b340d167c2f38aaa41
amatorin <Matorin.A.Aleks@sberbank.ru>
2020-05-11 23:59:55 +0300

    placeholder api changes

--- microconfig-core/src/main/java/io/microconfig/core/properties/resolvers/placeholder/strategies/environment/EnvironmentResolveStrategy.java
+++ microconfig-core/src/main/java/io/microconfig/core/properties/resolvers/placeholder/strategies/environment/EnvironmentResolveStrategy.java
@@ -10,12 +10,15 @@ import io.microconfig.core.properties.Property;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 
 import static io.microconfig.core.properties.ConfigFormat.PROPERTIES;
 import static io.microconfig.core.properties.PropertyImpl.property;
 import static java.util.Optional.empty;
+import static java.util.stream.Stream.of;
 
 @RequiredArgsConstructor
 public class EnvironmentResolveStrategy implements PlaceholderResolveStrategy {
@@ -31,11 +34,31 @@ public class EnvironmentResolveStrategy implements PlaceholderResolveStrategy {
         if (envProperty == null) return empty();
 
         DeclaringComponent component = placeholder.getReferencedComponent(root.getComponent());
-        Environment environment = getEnvironment(component.getEnvironment());
+        Environment environment = findEnv(placeholder, sourceOfValue, root, visited);
         if (environment == null) return empty();
 
         return envProperty.resolveFor(component.getComponent(), environment)
                 .map(value -> property(placeholder.getKey(), value, PROPERTIES, component));
+    }
+
+    private Environment findEnv(Placeholder p,
+                                DeclaringComponent sourceOfValue,
+                                DeclaringComponent root,
+                                Set<Placeholder> visited) {
+        if (p.isSelfReferenced(sourceOfValue)) {
+            return of(
+                    of(root),
+                    visited.stream().map(pl -> pl.getReferencedComponent("")),
+                    of(sourceOfValue)
+            ).flatMap(Function.identity())
+                    .map(DeclaringComponent::getEnvironment)
+                    .map(this::getEnvironment)
+                    .filter(Objects::nonNull)
+                    .findFirst().orElse(null);
+
+        }
+
+        return getEnvironment(p.getReferencedComponent("").getEnvironment());
     }
 
     private Environment getEnvironment(String environment) {
